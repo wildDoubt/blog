bf0de584ecc9f8882a762e96fa6f473ead85363a
Wonyeong Choi <coma01155@gmail.com>
2021-05-12 17:59:49 +0900

    update

--- update_theme.sh
+++
@@ -1,7 +0,0 @@
-echo -e "\033[0;32mUpdate themes...\033[0m"
-
-cd themes/hugo-clarity
-git remote add upstream https://github.com/chipzoller/hugo-clarity
-git fetch upstream
-git merge upstream/master
-git push
